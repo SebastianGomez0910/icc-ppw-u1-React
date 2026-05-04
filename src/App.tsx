7569c0c08f73626a1d29822ed1de321e586a94c5
Sebastian Gomez <sebgomez395@gmail.com>
2026-05-04 10:58:38 -0500

    chore: configuracion inicial del proyecto ReactStore con Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>ReactStore</h1>
+      <p>Proyecto en construccion...</p>
+    </div>
+  )
+}
+
+export default App
